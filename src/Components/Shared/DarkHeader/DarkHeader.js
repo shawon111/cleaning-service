4a33bde697ac146d030fd4703fec18eb7214772a
shawon ahmmed <shawonahmmed08509@gmail.com>
2021-12-27 23:57:17 +0600

    first commit

---
+++ src/Components/Shared/DarkHeader/DarkHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DarkHeader = () => {
+    return (
+        <div>
+            <h2>Dark header</h2>
+        </div>
+    );
+};
+
+export default DarkHeader;
---
+++ src/Components/Shared/LightHeader/LightHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LightHeader = () => {
+    return (
+        <div>
+            <h2>light header</h2>
+        </div>
+    );
+};
+
+export default LightHeader;
